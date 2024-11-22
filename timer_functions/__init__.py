494be0c656295e9da71c8ae263f2bea419039958
Etienne Soguel <etienne.soguel@gmail.com>
2024-11-22 17:07:53 +0100

    Updated init

--- timer_functions/__init__.py
+++ timer_functions/__init__.py
@@ -1,7 +1,23 @@
 import logging
-import datetime
 import azure.functions as func
 
+def main(timer: func.TimerRequest) -> None:
+    try:
+        # Prüfen, ob der Timer-Trigger korrekt initialisiert wurde
+        if timer.schedule_status:
+            utc_timestamp = timer.schedule_status.last
+            logging.info(f"Timer trigger executed at {utc_timestamp}")
+        else:
+            logging.warning("Timer schedule_status is None.")
+        
+    except Exception as e:
+        logging.error(f"An error occurred in the timer function: {str(e)}")
+
+
+        
+
+
+"""
 # Beispiel für die function, die im Azure Function Timer-Trigger ausgeführt wird
 def example_function():
     try:
@@ -26,3 +42,5 @@ def main(timer: func.TimerRequest) -> None:
     # Aufruf der example_function
     result = example_function()
     logging.info(f"example_function hat folgendes zurückgegeben: {result}")
+    
+"""
