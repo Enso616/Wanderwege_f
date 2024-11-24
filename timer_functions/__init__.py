dc2f985069b467ec08fe2671356449dc02bce88f
Etienne Soguel <etienne.soguel@gmail.com>
2024-11-24 11:09:13 +0100

    Update __init__.py

--- timer_functions/__init__.py
+++ timer_functions/__init__.py
@@ -12,35 +12,3 @@ def main(timer: func.TimerRequest) -> None:
         
     except Exception as e:
         logging.error(f"An error occurred in the timer function: {str(e)}")
-
-
-        
-
-
-"""
-# Beispiel für die function, die im Azure Function Timer-Trigger ausgeführt wird
-def example_function():
-    try:
-        # Deine Berechnungen
-        a = 5
-        b = 10
-        result = a + b
-        logging.info(f"Demo Calculation: {a} + {b} = {result}")
-        return "Hello from example_function!"
-    except Exception as e:
-        logging.error(f"Error in example_function: {str(e)}")
-        return "Error"
-
-# Hauptfunktion, die bei einem Timer-Trigger von Azure aufgerufen wird
-def main(timer: func.TimerRequest) -> None:
-    """
-    Diese Funktion wird durch den Timer-Trigger von Azure aufgerufen.
-    """
-    utc_timestamp = timer.schedule_status.last
-    logging.info(f"Timer trigger executed at {utc_timestamp}")
-
-    # Aufruf der example_function
-    result = example_function()
-    logging.info(f"example_function hat folgendes zurückgegeben: {result}")
-    
-"""
