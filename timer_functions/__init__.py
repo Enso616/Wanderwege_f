7915e2fe45960258cd073a33108c45f7d95e651f
Etienne Soguel <etienne.soguel@gmail.com>
2024-11-24 11:39:00 +0100

    Update __init__.py

--- timer_functions/__init__.py
+++ timer_functions/__init__.py
@@ -1,14 +1,14 @@
+import datetime
 import logging
+
 import azure.functions as func
 
-def main(timer: func.TimerRequest) -> None:
-    try:
-        # Prüfen, ob der Timer-Trigger korrekt initialisiert wurde
-        if timer.schedule_status:
-            utc_timestamp = timer.schedule_status.last
-            logging.info(f"Timer trigger executed at {utc_timestamp}")
-        else:
-            logging.warning("Timer schedule_status is None.")
-        
-    except Exception as e:
-        logging.error(f"An error occurred in the timer function: {str(e)}")
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
