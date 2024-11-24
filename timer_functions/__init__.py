0fc76a7ba89c35d0defe523d304212a1a3fcaf60
Etienne Soguel <etienne.soguel@gmail.com>
2024-11-24 11:40:19 +0100

    Update __init__.py

--- timer_functions/__init__.py
+++ timer_functions/__init__.py
@@ -7,8 +7,10 @@ import azure.functions as func
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
+    logging.info('HELLO DUDE WAKE UP?????????????')        
 
     if mytimer.past_due:
         logging.info('The timer is past due!')
+        logging.info('HELLO?????????????')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
